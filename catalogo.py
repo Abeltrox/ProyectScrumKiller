d074d34bea056310b2b0aa1950bbed1c42ffe0d8
andresguerra321 <137118422+andresguerra321@users.noreply.github.com>
2026-02-16 22:35:49 -0500

    feat: :sparkles: Cargar Productos
    Cargar productos desde la base de datos

---
+++ catalogo.py
@@ -0,0 +1,42 @@
+import json
+import csv
+import os
+
+# CARGAR PRODUCTOS DESDE BASE DE DATOS
+def cargar_productos_bd(archivo_productos):
+    """
+    Obtiene la información de productos desde la base de datos (archivo)
+    Criterio: La información se obtiene desde la base de datos
+    """
+    productos = []
+
+    try:
+        with open(archivo_productos, "r", encoding="utf-8") as archivo:
+            for linea in archivo:
+                partes = linea.strip().split(",")
+                
+                # id, nombre, descripcion, precio, moneda, stock, categoria, client_id
+                if len(partes) == 8:
+                    producto = {
+                        "id": partes[0],
+                        "nombre": partes[1],
+                        "descripcion": partes[2],
+                        "precio": float(partes[3]),
+                        "moneda": partes[4],
+                        "stock": int(partes[5]),
+                        "categoria": partes[6],
+                        "client_id": partes[7]
+                    }
+                    
+                    productos.append(producto)
+
+    except FileNotFoundError:
+        print("  No hay productos disponibles en la base de datos.")
+    except ValueError as e:
+        print(f"Error de formato en la base de datos: {e}")
+    except Exception as e:
+        print(f"Error al cargar productos: {e}")
+
+    return productos
+
+
