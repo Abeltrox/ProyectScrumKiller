6c13cc0528fc665ecfcb5c9c17c33eae488554bd
CondezPancake <148305130+CondezPancake@users.noreply.github.com>
2026-02-16 20:01:40 -0500

    feat: :sparkles: Creacion Repositorio Cliente
    leer base datos, escribir, guardar, buscar CLIENTE
    
    repositories-client-repository

---
+++ repositories/client_repository.py
@@ -0,0 +1,35 @@
+import json
+from config.settings import RUTA_BASEDATOS
+from utils.logger import logger
+
+
+class RepositorioCliente:
+
+    def _leer_base_de_datos(self):
+        try:
+            with open(RUTA_BASEDATOS, "r") as archivo:
+                return json.load(archivo)
+        except:
+            return {
+                "clientes": [],
+                "usuarios": [],
+                "productos": [],
+                "ordenes": []
+            }
+
+    def _escribir_base_de_datos(self, datos):
+        with open(RUTA_BASEDATOS, "w") as archivo:
+            json.dump(datos, archivo, indent=4)
+
+    def guardar(self, cliente):
+        datos = self._leer_base_de_datos()
+        datos["clientes"].append(cliente.a_diccionario())
+        self._escribir_base_de_datos(datos)
+        logger.info("Cliente guardado correctamente")
+
+    def buscar_por_correo(self, correo):
+        datos = self._leer_base_de_datos()
+        for cliente in datos["clientes"]:
+            if cliente["correo"] == correo:
+                return cliente
+        return None
