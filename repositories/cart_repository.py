029ede186d6d3e75e75eb10b47f5c87fc15f2ffb
andresguerra321 <137118422+andresguerra321@users.noreply.github.com>
2026-02-17 19:52:17 -0500

    feat: :sparkles: Guardado y carga

--- repositories/cart_repository.py
+++ repositories/cart_repository.py
@@ -29,3 +29,50 @@ def cargar_carrito(archivo_carrito, usuario_id):
     # Filtrar solo items del usuario actual
     mi_carrito = [item for item in carritos if item["usuario_id"] == usuario_id]
     return mi_carrito, carritos
+
+def guardar_carrito(archivo_carrito, todos_carritos):
+    """
+    Guarda todos los carritos en la base de datos
+
+    """
+    try:
+        with open(archivo_carrito, "w", encoding="utf-8") as archivo:
+            for item in todos_carritos:
+                archivo.write(
+                    f"{item['usuario_id']},{item['producto_id']},"
+                    f"{item['cantidad']},{item['fecha_agregado']}\n"
+                )
+        print("✓ Carrito guardado.")
+    except Exception as e:
+        print(f" Error al guardar carrito: {e}")
+
+
+def cargar_productos_para_carrito(archivo_productos):
+    """
+    Carga productos como diccionario para acceso rápido por ID
+    """
+    productos = {}
+
+    try:
+        with open(archivo_productos, "r", encoding="utf-8") as archivo:
+            for linea in archivo:
+                partes = linea.strip().split(",")
+                
+                if len(partes) == 8:
+                    productos[partes[0]] = {
+                        "id": partes[0],
+                        "nombre": partes[1],
+                        "descripcion": partes[2],
+                        "precio": float(partes[3]),
+                        "moneda": partes[4],
+                        "stock": int(partes[5]),
+                        "categoria": partes[6],
+                        "client_id": partes[7]
+                    }
+
+    except FileNotFoundError:
+        print("  No hay productos disponibles.")
+    except Exception as e:
+        print(f"Error al cargar productos: {e}")
+
+    return productos
